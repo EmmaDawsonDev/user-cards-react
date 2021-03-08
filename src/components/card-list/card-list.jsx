babe4587f1ff75aa050efa1e5ec60b7ce483750b
Emma Dawson <emma.l.dawson@gmail.com>
2021-03-08 09:20:25 +0100

    Initial set up

---
+++ src/components/card-list/card-list.jsx
@@ -0,0 +1,13 @@
+
+import Card from "../card/card"
+
+function Cardlist() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Card />
+    </div>
+  )
+}
+
+export default Cardlist
---
+++ src/components/card/card.jsx
@@ -0,0 +1,10 @@
+
+function Card() {
+  return (
+    <div>
+      <h1>I'm a card</h1>
+    </div>
+  )
+}
+
+export default Card
